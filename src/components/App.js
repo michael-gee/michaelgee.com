6464f99d910db6c19c0605763648ec33024a41be
Michael Gee <michael.gee@coreview.com>
2018-12-31 09:34:34 -0500

    component/container folder structure additions - prettierrc implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import logo from '../logo.svg'
+
+import './App.css'
 
 class App extends Component {
   render() {
@@ -14,8 +16,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
