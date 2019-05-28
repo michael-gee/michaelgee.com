3cf58a17a959dd57bee5dd7823ade42aee525f5e
Michael Gee <michael.gee@coreview.com>
2019-05-28 12:06:33 -0400

    useEffect implemenation and comment details

--- src/components/Hooks/Hooks.js
+++ src/components/Hooks/Hooks.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './Hooks.css'
 
 const HooksComponent = () => {
   const [count, setCount] = useState(0)
+  const [isMessageShown, showMessage] = useState(false)
+
+  // passed count as the second parameter tells this effect only to run if the count variable has changed
+  useEffect(() => {
+    console.log('count effect')
+    document.title = `You clicked ${count} times`
+
+    // RETURNing a method within a useEffect is a way of using an effect which will also need clean up (unsubscribing to something when the component unmounts)
+    // return () => { unsubsribe() }
+  }, [count])
+
+  // side note: (memory leaks) memory leaks happen when component state updates happen in a component that is unmounted: for example when this.setState is in a callback or after an async method in mount or update which does not complete execution. this will result in the setState being kept in memory and can cause problems in the application.
 
   return (
     <div id="rs-hooks-container">
@@ -16,6 +28,11 @@ const HooksComponent = () => {
         <button className="rs-hooks-counter-btn" onClick={() => setCount(count - 1)}>
           Decrement
         </button>
+      </div>
+
+      <div id="rs-hooks-hiddenMessage-example">
+        {isMessageShown && <div>hello world</div>}
+        {!isMessageShown && <button onClick={() => showMessage(true)}>Show Message</button>}
       </div>
     </div>
   )
