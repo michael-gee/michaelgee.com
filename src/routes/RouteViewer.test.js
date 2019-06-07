4d448bb48200a386762147f7febf90065949e397
Michael Gee <michael.gee@coreview.com>
2019-06-07 11:35:37 -0400

    add tests to RouteViewer

--- src/routes/RouteViewer.test.js
+++ src/routes/RouteViewer.test.js
@@ -4,14 +4,16 @@ import Adapter from 'enzyme-adapter-react-16'
 
 import RouteViewer from './RouteViewer'
 
+import constants from '../constants'
+
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('<RouteViewer />', () => {
   let wrapper, mockHistory, mockLocation, mockMatch
 
   beforeEach(() => {
-    mockHistory = {}
-    mockLocation = {}
+    mockHistory = { replace: jest.fn() }
+    mockLocation = { pathname: constants.routePaths.homepage }
     mockMatch = {}
 
     wrapper = shallow(<RouteViewer history={mockHistory} location={mockLocation} match={mockMatch} />)
@@ -19,5 +21,38 @@ describe('<RouteViewer />', () => {
 
   it('should renders without error with its expected props', () => {
     expect(wrapper.find('[data-test="rs-routeViewer"]').length).toEqual(1)
+  })
+
+  describe('when the current location is the homepage', () => {
+    it('should NOT render the "Back to Home" button', () => {
+      // initial location is set to the homepage in the beforeEach
+
+      expect(wrapper.find('[data-test="rs-routeViewer-navBtn"]').length).toEqual(0)
+    })
+  })
+
+  describe('when the current location is any other route than the homepage', () => {
+    it('should render the "Back to Home" button', () => {
+      expect(wrapper.find('[data-test="rs-routeViewer-navBtn"]').length).toEqual(0)
+
+      mockLocation = { pathname: constants.routePaths.counter }
+      // current location route set to the counter route
+      wrapper.setProps({ location: mockLocation })
+
+      expect(wrapper.find('[data-test="rs-routeViewer-navBtn"]').length).toEqual(1)
+    })
+
+    it('should navigate user back to the homepage when the "Back to Home" button is clicked', () => {
+      expect(mockHistory.replace).not.toHaveBeenCalled()
+
+      mockLocation = { pathname: constants.routePaths.counter }
+      // current location route set to the counter route
+      wrapper.setProps({ location: mockLocation })
+
+      const NavButton = wrapper.find('[data-test="rs-routeViewer-navBtn"]')
+      NavButton.props().onClick()
+
+      expect(mockHistory.replace).toHaveBeenCalledWith(constants.routePaths.homepage)
+    })
   })
 })
