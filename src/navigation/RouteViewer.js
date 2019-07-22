1136b0bfc4090b81d59403016119e7f14e54b055
Michael Gee <michael.gee@coreview.com>
2019-07-22 12:34:29 -0400

    comment out Suspense code until API is more concrete

--- src/navigation/RouteViewer.js
+++ src/navigation/RouteViewer.js
@@ -1,11 +1,11 @@
-import React, { useRef, useEffect, Suspense, lazy } from 'react'
+import React, { useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Route } from 'react-router-dom'
 
 import Button from '@material-ui/core/Button'
 import HomeIcon from '@material-ui/icons/Home'
 
-import Loading from '../components/Loading'
+// import Loading from '../components/Loading'
 
 import routes from './routes'
 import constants from '../constants'
@@ -35,23 +35,23 @@ const RouteViewer = props => {
 
   return (
     <div className={classes.routeViewerContainer} data-test="rs-routeViewer">
-      {/* REPLACE THIS WITH CUSTOM SPINNER COMPONENT */}
-      <Suspense fallback={<Loading />}>
-        {props.location.pathname !== '/' && (
-          <Button
-            onClick={() => triggerNavigationChange(constants.navigation.routePaths.homepage)}
-            color="primary"
-            variant="contained"
-            data-test="rs-routeViewer-navBtn"
-          >
-            <HomeIcon />
-            <span className={classes.backNavButtonText}>Back to Home</span>
-          </Button>
-        )}
+      {/* Implement Suspense/code splitting when react API is more declared and ready */}
+      {/* <Suspense fallback={<Loading />}> */}
+      {props.location.pathname !== '/' && (
+        <Button
+          onClick={() => triggerNavigationChange(constants.navigation.routePaths.homepage)}
+          color="primary"
+          variant="contained"
+          data-test="rs-routeViewer-navBtn"
+        >
+          <HomeIcon />
+          <span className={classes.backNavButtonText}>Back to Home</span>
+        </Button>
+      )}
 
-        <Route exact path={constants.navigation.routePaths.homepage} component={routes.Homepage} />
-        <Route path={constants.navigation.routePaths.counter} component={routes.Counter} />
-      </Suspense>
+      <Route exact path={constants.navigation.routePaths.homepage} component={routes.Homepage} />
+      <Route path={constants.navigation.routePaths.counter} component={routes.Counter} />
+      {/* </Suspense> */}
     </div>
   )
 }
--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,6 +1,16 @@
-import { lazy } from 'react'
+// Implement Suspense/code splitting when react API is more declared and ready
+
+// import { lazy } from 'react'
+
+// export default {
+//   Homepage: lazy(() => import('../routes/Homepage')),
+//   Counter: lazy(() => import('../routes/Counter'))
+// }
+
+import Homepage from '../routes/Homepage'
+import Counter from '../routes/Counter'
 
 export default {
-  Homepage: lazy(() => import('../routes/Homepage')),
-  Counter: lazy(() => import('../routes/Counter'))
+  Homepage,
+  Counter
 }
