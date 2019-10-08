5ce31f9b330cc90690c32e532c2013bfe9c28751
Michael Gee <michaelgee221@gmail.com>
2019-10-08 13:27:09 -0400

    isOpen configured in TreeList items -  useEffect can be moved to bottom of component in private methods if it is called above the render

--- src/features/TreeList/TreeList.js
+++ src/features/TreeList/TreeList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { ActionButton } from 'office-ui-fabric-react'
+import { ActionButton, IconButton } from 'office-ui-fabric-react'
 
 import data from './static'
 
@@ -11,41 +11,7 @@ const TreeList = props => {
   const [renderedTreeData, setRenderedTreeData] = useState([])
   const [currentlySelected, setCurrentlySelected] = useState(null)
 
-  useEffect(() => {
-    const flattenedData = flattenAndSortTreeData()
-    setRenderedTreeData(flattenedData)
-
-    function flattenAndSortTreeData() {
-      const arr = []
-
-      // flattens array to just organizationalUnites
-      if (treeData.length > 0) {
-        treeData.forEach(item => {
-          if (item.organizationalUnits.length > 0) {
-            item.organizationalUnits.forEach(unit => {
-              arr.push(unit)
-            })
-          }
-        })
-      }
-
-      // places children items directly under the parent in flattened array
-      arr.forEach(item => {
-        if (item.parent) {
-          const idArr = arr.map(arrItem => arrItem.id)
-
-          const currentItem = item
-          const currentItemIndex = idArr.indexOf(item.id)
-          const parentIndex = idArr.indexOf(item.parent)
-
-          arr.splice(currentItemIndex, 1)
-          arr.splice(parentIndex + 1, 0, currentItem)
-        }
-      })
-
-      return arr
-    }
-  }, [treeData])
+  _configureTreeData()
 
   return (
     <div id="rs-treeList-container">
@@ -59,8 +25,18 @@ const TreeList = props => {
             {renderedTreeData.map(item => {
               return (
                 <li className="rs-treeList-item" style={{ paddingLeft: (item.depth - 1) * 40 }} key={item.id}>
+                  {item.hasChild ? (
+                    <IconButton
+                      className="rs-treeList-caret-icon"
+                      iconProps={{ iconName: item.isOpen ? 'CaretSolid' : 'CaretHollow' }}
+                      // onClick={() => setCurrentlySelected(item.distinguishedName)}
+                    />
+                  ) : (
+                    <div style={{ padding: '0 16px' }} />
+                  )}
+
                   <ActionButton
-                    className="rs-treeList-item-icon"
+                    className="rs-treeList-folder-icon"
                     iconProps={{ iconName: item.isOpen ? 'OpenFolderHorizontal' : 'FolderHorizontal' }}
                     disabled={!item.userFilterEnabled}
                     onClick={() => setCurrentlySelected(item.distinguishedName)}
@@ -76,9 +52,44 @@ const TreeList = props => {
     </div>
   )
 
-  // function _configureTreeData(item) {
-  //   setTreeData([...treeData, data[treeData.length]])
-  // }
+  function _configureTreeData() {
+    useEffect(() => {
+      const flattenedData = flattenAndSortTreeData()
+      setRenderedTreeData(flattenedData)
+
+      function flattenAndSortTreeData() {
+        const arr = []
+
+        // flattens array to just organizationalUnites
+        if (treeData.length > 0) {
+          treeData.forEach(item => {
+            if (item.organizationalUnits.length > 0) {
+              item.organizationalUnits.forEach(unit => {
+                arr.push(unit)
+              })
+            }
+          })
+        }
+
+        // places children items directly under the parent in flattened array
+        arr.forEach(item => {
+          if (item.parent) {
+            const idArr = arr.map(arrItem => arrItem.id)
+
+            const currentItem = item
+            const currentItemIndex = idArr.indexOf(item.id)
+            const parentIndex = idArr.indexOf(item.parent)
+
+            arr.splice(currentItemIndex, 1)
+            arr.splice(parentIndex + 1, 0, currentItem)
+            arr[parentIndex].isOpen = true
+          }
+        })
+
+        return arr
+      }
+    }, [treeData])
+  }
 }
 
 export default TreeList
