9c702d185f96f3f1712ec372cf88f93ee2992595
Michael Gee <michaelgee221@gmail.com>
2019-10-08 12:54:33 -0400

    currently selected option added to TreeList

--- src/features/TreeList/TreeList.js
+++ src/features/TreeList/TreeList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { IconButton } from 'office-ui-fabric-react'
+import { ActionButton } from 'office-ui-fabric-react'
 
 import data from './static'
 
@@ -9,6 +9,7 @@ import './TreeList.css'
 const TreeList = props => {
   const [treeData, setTreeData] = useState(data)
   const [renderedTreeData, setRenderedTreeData] = useState([])
+  const [currentlySelected, setCurrentlySelected] = useState(null)
 
   useEffect(() => {
     const flattenedData = flattenAndSortTreeData()
@@ -51,21 +52,26 @@ const TreeList = props => {
       {treeData.length < 1 ? (
         <div>No data was found</div>
       ) : (
-        <ul className="rs-treeList-list">
-          {renderedTreeData.map(item => {
-            return (
-              <li className="rs-treeList-item" style={{ paddingLeft: (item.depth - 1) * 40 }} key={item.id}>
-                <IconButton
-                  className="rs-treeList-item-icon"
-                  iconProps={{ iconName: item.isOpen ? 'OpenFolderHorizontal' : 'FolderHorizontal' }}
-                  disabled={!item.hasChild && !item.userFilterEnabled}
-                  // onClick={() => _configureTreeData(item)}
-                />
-                <span className="rs-treeList-item-text">{item.name}</span>
-              </li>
-            )
-          })}
-        </ul>
+        <div>
+          <div>Currently Selected: {currentlySelected ? `${currentlySelected}` : 'N/A'}</div>
+
+          <ul className="rs-treeList-list">
+            {renderedTreeData.map(item => {
+              return (
+                <li className="rs-treeList-item" style={{ paddingLeft: (item.depth - 1) * 40 }} key={item.id}>
+                  <ActionButton
+                    className="rs-treeList-item-icon"
+                    iconProps={{ iconName: item.isOpen ? 'OpenFolderHorizontal' : 'FolderHorizontal' }}
+                    disabled={!item.userFilterEnabled}
+                    onClick={() => setCurrentlySelected(item.distinguishedName)}
+                  >
+                    {item.name}
+                  </ActionButton>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       )}
     </div>
   )
