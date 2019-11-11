c419a17eed3a0ccf2c8da812e952532accbe4e89
Michael Gee <michaelgee221@gmail.com>
2019-11-11 16:17:25 -0500

    fix RouteHeader tests

--- src/navigation/RouteHeader/RouteHeader.test.js
+++ src/navigation/RouteHeader/RouteHeader.test.js
@@ -3,21 +3,16 @@ import { render, fireEvent } from '@testing-library/react'
 
 import RouteHeader from './RouteHeader'
 
-import constants from '../../constants'
-
-jest.mock('../../hooks/useAppCommand', () => {
-  return jest.fn().mockImplementation(() => {
-    return {}
-  })
-})
+import { RS_ROUTE_PATHS } from '../../constants/navigation'
 
 describe('<RouteHeader />', () => {
-  let wrapper, mockCurrentRoute
+  let wrapper, mockPathname, mockGoBack
 
   beforeEach(() => {
-    mockCurrentRoute = '/simple-hooks-counter'
+    mockPathname = RS_ROUTE_PATHS.counter
+    mockGoBack = jest.fn()
 
-    wrapper = render(<RouteHeader currentRoute={mockCurrentRoute} />)
+    wrapper = render(<RouteHeader location={{ pathname: mockPathname }} history={{ goBack: mockGoBack }} />)
   })
 
   it('should render without error', () => {
@@ -27,9 +22,9 @@ describe('<RouteHeader />', () => {
   })
 
   it('should NOT render the home navigation icon button when the current route is the homepage', () => {
-    // homepage is NOT the currentRoute by default: currentRoute = /simple/hooks/counter
     const { queryByTestId, rerender } = wrapper
-    rerender(<RouteHeader currentRoute={constants.navigation.routePaths.homepage} />)
+    // props.location.pathname set to homepage
+    rerender(<RouteHeader location={{ pathname: RS_ROUTE_PATHS.homepage }} history={{ goBack: mockGoBack }} />)
 
     expect(queryByTestId('rs-header-iconBtn')).toBeNull()
   })
@@ -40,25 +35,24 @@ describe('<RouteHeader />', () => {
     expect(queryByTestId('rs-header-iconBtn')).toBeTruthy()
   })
 
-  // @@@@@ fix test
-  xit('should navigate to the homepage when the navigation icon button is clicked', () => {
+  it('should navigate back to the homepage when the navigation icon button is clicked', () => {
     const { queryByTestId } = wrapper
     const NavigationIconButton = queryByTestId('rs-header-iconBtn')
 
-    expect(mockNavigateTo).not.toHaveBeenCalled()
+    expect(mockGoBack).not.toHaveBeenCalled()
     fireEvent.click(NavigationIconButton)
-    expect(mockNavigateTo).toHaveBeenCalledWith(constants.navigation.routePaths.homepage)
+    expect(mockGoBack).toHaveBeenCalled()
   })
 
   it('should render the title dependent on the currentRoute', () => {
     const { queryByText, rerender } = wrapper
     expect(queryByText('Simple Hooks Counter')).toBeTruthy()
 
-    rerender(<RouteHeader currentRoute={constants.navigation.routePaths.homepage} />)
+    rerender(<RouteHeader location={{ pathname: RS_ROUTE_PATHS.homepage }} history={{ goBack: mockGoBack }} />)
     expect(queryByText('Simple Hooks Counter')).toBeNull()
     expect(queryByText('React Sandbox')).toBeTruthy()
 
-    rerender(<RouteHeader currentRoute={constants.navigation.routePaths.dataTable} />)
+    rerender(<RouteHeader location={{ pathname: RS_ROUTE_PATHS.dataTable }} history={{ goBack: mockGoBack }} />)
     expect(queryByText('ReactSandbox')).toBeNull()
     expect(queryByText('React Table V7')).toBeTruthy()
   })
