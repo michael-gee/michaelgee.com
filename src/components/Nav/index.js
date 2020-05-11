32f43304d9216a15355ed2dca955cf3d0500e7f1
Michael Gee <michaelgee221@gmail.com>
2020-05-11 13:40:04 -0500

    Nav initial implementation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,48 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 
+import { Button, Icon } from 'semantic-ui-react'
+
+import styles from './Nav.module.css'
+
 export const Nav = props => {
+  const navButtons = _configureNavButtons()
   const router = useRouter()
 
   return (
-    <header>
-      <nav>
-        <button onClick={() => _handleNavChange('/')}>About Me</button>
-
-        <button onClick={() => _handleNavChange('/contact')}>Contact</button>
+    <header id={styles.header}>
+      <nav id={styles.nav}>
+        {navButtons.map(btn => (
+          <Button
+            icon
+            onClick={() => _handleNavChange(btn.pathname)}
+            size="huge"
+            id={btn.pathname === router.pathname ? styles.selected : undefined}
+            className={styles.navBtn}
+            key={btn.icon}
+          >
+            <Icon name={btn.icon} />
+            <span>{btn.text}</span>
+          </Button>
+        ))}
       </nav>
     </header>
   )
+
+  function _configureNavButtons() {
+    return [
+      {
+        icon: 'user',
+        text: 'About',
+        pathname: '/'
+      },
+      {
+        icon: 'mail',
+        text: 'Contact',
+        pathname: '/contact'
+      }
+    ]
+  }
 
   function _handleNavChange(pathname) {
     if (pathname !== router.pathname) router.push(pathname)
