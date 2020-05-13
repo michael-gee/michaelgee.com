ff7f36b10e73988ff7ec381aa1b6cbae68634555
Michael Gee <michaelgee221@gmail.com>
2020-05-13 08:32:14 -0400

    Contact page UI

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import { Form, Button, Input, TextArea, Icon } from 'semantic-ui-react'
 import { Profile } from '../../components/Profile'
 import { Nav } from '../../components/Nav'
+
+import styles from './Contact.module.css'
 
 export const Contact = props => {
   return (
@@ -10,9 +13,42 @@ export const Contact = props => {
 
       <Profile />
 
-      <section id="page-body">
-        <h1>Contact</h1>
-      </section>
+      <div id="page-body">
+        <section>
+          <h2 id={styles.title}>Get In Touch</h2>
+
+          <Form id={styles.form}>
+            <div id={styles.userInfo}>
+              <Input placeholder="Full Name" icon="user" iconPosition="left" />
+
+              <Input placeholder="Email Address" icon="at" iconPosition="left" />
+            </div>
+
+            <TextArea placeholder="Your Message" rows="5" />
+
+            <Button id={styles.submitBtn}>
+              <Icon name="send" />
+              Send Message
+            </Button>
+          </Form>
+        </section>
+
+        <section>
+          <h2 id={styles.otherTitle}>Other Ways To Contact</h2>
+
+          <div id={styles.otherContact}>
+            <div>
+              <Icon name="at" size="large" />
+              <span>michaelgee221@gmail.com</span>
+            </div>
+
+            <div>
+              <Icon name="discord" size="large" />
+              <span>michaelg33#9503</span>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
