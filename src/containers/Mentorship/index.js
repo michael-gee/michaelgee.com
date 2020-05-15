7fd4bae2f0fce63877c937da9be10590a4650480
Michael Gee <michaelgee221@gmail.com>
2020-05-15 16:03:45 -0400

    initial Mentorship page commit

--- src/containers/Mentorship/index.js
+++ src/containers/Mentorship/index.js
@@ -1,16 +1,46 @@
 import Head from 'next/head'
-import { Profile } from '../../components/Profile'
+import { Icon } from 'semantic-ui-react'
 import { Nav } from '../../components/Nav'
+
+import styles from './Mentorship.module.css'
 
 export const Mentorship = () => {
   return (
     <main id="page">
       <Nav />
 
-      <Profile />
+      <div id={styles.container}>
+        <Icon name="rocket" size="massive" id={styles.rocketIcon} />
+        <h1 id={styles.title}>Accelerate your software development journey</h1>
 
-      <div className="page-body">
-        <h1>Mentorship</h1>
+        <h2 id={styles.mainSubTitle}>
+          Interested in getting started in software development? Tired of getting stuck or want to
+          rapidly speed up your growth and reach your end goals? Let's get started today!
+        </h2>
+
+        <section>
+          <h2 className={styles.subTitle}>Benefits</h2>
+
+          <p>Icons with text (6)</p>
+        </section>
+
+        <section>
+          <h2 className={styles.subTitle}>How It Works</h2>
+
+          <p>Numbered list or stepper explaining process</p>
+        </section>
+
+        <section>
+          <h2 className={styles.subTitle}>Target Audience</h2>
+
+          <p>Bulleted list (primary green bullets)</p>
+        </section>
+
+        <section>
+          <h2 className={styles.subTitle}>Pricing</h2>
+
+          <p>3 column pricing model</p>
+        </section>
       </div>
     </main>
   )
