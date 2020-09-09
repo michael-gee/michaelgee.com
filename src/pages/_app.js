69b600717532d19c25bbc91803c7cde01d7e51d3
Michael Gee <michaelgee221@gmail.com>
2020-09-08 21:58:11 -0400

    add background animation effect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,14 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <div id="bg-back" />
+      <div id="bg-front" />
+    </>
+  )
 }
 
 export default App
