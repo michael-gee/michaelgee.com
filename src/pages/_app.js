129bcec63d5ddc07b387ab97408ba7ce28decac3
Michael Gee <michaelgee221@gmail.com>
2020-09-25 10:30:32 -0400

    add isLoading state to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
 import { Nav } from '../components/Nav'
 import { Profile } from '../components/Profile'
 
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
 const App = ({ Component, pageProps }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => setIsLoading(true))
+    router.events.on('routeChangeComplete', () => setIsLoading(false))
+    router.events.on('routeChangeError', () => setIsLoading(false))
+  }, [])
+
+  // Add loading spinner if isLoading is true
+
   return (
     <main id="page">
       <Nav />
@@ -12,7 +26,7 @@ const App = ({ Component, pageProps }) => {
       <div className="page-body">
         <Profile />
 
-        <Component {...pageProps} />
+        {!isLoading && <Component {...pageProps} />}
       </div>
 
       <div id="bg-back" />
