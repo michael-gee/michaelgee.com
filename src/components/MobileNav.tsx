6c73fc5d53a8cad61164e53358dd4017f0c8a28b
Michael Gee <michaelgee221@gmail.com>
2021-04-13 21:36:36 -0400

    another change

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -39,8 +39,8 @@ export const MobileNav = () => {
 			bottom={0}
 			right={0}
 			left={0}
-			pt="24px"
-			pb="32px"
+			pt="32px"
+			pb="40px"
 			bg={useColorModeValue('white', 'gray.800')}
 			borderTopWidth="2px"
 			borderTopColor={useColorModeValue('gray.100', 'gray.700')}
