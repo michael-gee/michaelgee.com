32cc7303d205694b551f7b5b905e6583220e8337
Michael Gee <michaelgee221@gmail.com>
2021-06-09 08:10:09 -0400

    site image now hosted instead of static

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -16,13 +16,16 @@ export enum PageTypes {
 
 export const PageHead = (props: Props) => {
 	const pageType = props.type ? props.type : PageTypes.website
-	const pageImage = props.image ? props.image : '/images/michaelgee-banner.jpg'
+	const pageImage = props.image
+		? props.image
+		: 'https://res.cloudinary.com/michaelgee/image/upload/v1623239996/michaelgee-banner_vmiduw.jpg'
 
 	return (
 		<Head>
 			<title>{props.title}</title>
 			<meta property="title" content={props.title} key="title" />
 			<meta name="description" content={props.description} key="description" />
+			<meta name="image" content={pageImage} key="image" />
 			<meta name="author" content="Michael Gee" />
 			<meta property="og:locale" content="en_US" />
 			<meta property="og:site_name" content="Michael Gee's Website" />
@@ -34,9 +37,10 @@ export const PageHead = (props: Props) => {
 
 			<meta property="twitter:title" content={props.title} key="twitter:title" />
 			<meta property="twitter:description" content={props.description} key="twitter:description" />
-			<meta name="twitter:creator" content="Michael Gee" />
+			<meta name="twitter:creator" content="@michaelgee7" />
 			<meta property="twitter:url" content={props.url} key="twitter:url" />
 			<meta property="twitter:image" content={pageImage} key="twitter:image" />
+			<meta property="twitter:card" content="summary_large_image" />
 
 			{props.children}
 		</Head>
