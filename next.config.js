4b39c4f5e2c060df4b9ccf4ff5c9828e20c76299
Michael Gee <michaelgee221@gmail.com>
2022-07-14 00:42:14 -0400

    mdx implementation initial commit

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
+const withMDX = require('@next/mdx')({
+	extension: /\.mdx?$/
+})
 
-module.exports = (phase) => {
-	// when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
-	const isDev = phase === PHASE_DEVELOPMENT_SERVER
-	// when `next build` or `npm run build` is used
-	const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
-	// when `next build` or `npm run build` is used
-	const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-
-	return {
-		env: {
-			SUBSCRIPTION_REDIRECT_URL: isDev ? 'localhost:3000/success' : 'https://michaelgee.com/success'
-		},
-		images: {
-			domains: ['res.cloudinary.com']
-		}
+module.exports = withMDX({
+	pageExtensions: ['ts', 'tsx', 'mdx'],
+	images: {
+		domains: ['res.cloudinary.com']
 	}
-}
+})
