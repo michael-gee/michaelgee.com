f68f5a66e0324b62eefc4c7d4e234e228e7f20b2
Michael Gee <michaelgee221@gmail.com>
2022-10-05 23:15:40 -0400

    astro initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://michaelgee.com',
+	integrations: [image(), mdx(), sitemap()]
+});
