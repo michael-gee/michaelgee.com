55a88a9450e0f3b4aef19016ef6e327cf11d0093
Michael Gee <michaelgee221@gmail.com>
2022-10-19 22:35:18 -0400

    add tailwind css back into project & implement initial styles into the Header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://michaelgee.com',
-	integrations: [image({ serviceEntryPoint: '@astrojs/image/sharp' }), mdx(), sitemap()]
+	integrations: [
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp'
+		}),
+		mdx(),
+		sitemap(),
+		tailwind()
+	]
 });
