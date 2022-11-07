cf44bd5164c1b5d80b5915c1d083c6cc323f6061
Michael Gee <michaelgee221@gmail.com>
2022-11-07 10:02:00 -0500

    Tech section on homepage - node v16 package.lock changes

---
+++ src/constants/tech.ts
@@ -0,0 +1,81 @@
+export interface Tech {
+	name: string;
+	iconName: string;
+	iconPack: string;
+	iconColor: string;
+}
+
+export const tech: Tech[] = [
+	{
+		name: 'React',
+		iconName: 'react',
+		iconPack: 'mdi',
+		iconColor: '#129eca'
+	},
+	{
+		name: 'React Native',
+		iconName: 'react',
+		iconPack: 'mdi',
+		iconColor: '#129eca'
+	},
+	{
+		name: 'TypeScript',
+		iconName: 'language-typescript',
+		iconPack: 'mdi',
+		iconColor: '#3078C6'
+	},
+	{
+		name: 'JavaScript',
+		iconName: 'language-javascript',
+		iconPack: 'mdi',
+		iconColor: '#FCDC00'
+	},
+	{
+		name: 'Next.js',
+		iconName: 'nextjs',
+		iconPack: 'file-icons',
+		iconColor: '#fff'
+	},
+	{
+		name: 'Tailwind',
+		iconName: 'tailwind',
+		iconPack: 'mdi',
+		iconColor: '#38BDF8'
+	},
+	{
+		name: 'SCSS',
+		iconName: 'sass',
+		iconPack: 'mdi',
+		iconColor: '#CE649A'
+	},
+	{
+		name: 'CSS',
+		iconName: 'language-css3',
+		iconPack: 'mdi',
+		iconColor: '#264DE4'
+	},
+	{
+		name: 'AWS',
+		iconName: 'aws',
+		iconPack: 'mdi',
+		iconColor: '#EC902E'
+	},
+	{
+		name: 'PostgreSQL',
+		iconName: 'postgresql',
+		iconPack: 'simple-icons',
+		iconColor: '#336791'
+	},
+	{
+		name: 'Node.js',
+		iconName: 'nodejs',
+		iconPack: 'mdi',
+		iconColor: '#67A063'
+	},
+	{
+		name: 'Git',
+		iconName: 'git',
+		iconPack: 'mdi',
+		iconColor: '#F64D27'
+	}
+];
