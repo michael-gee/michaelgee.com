1bfac3096e10c805930d332f910f99e39e0478db
Michael Gee <michaelgee221@gmail.com>
2022-11-10 19:24:05 -0500

    add astrojs/react integrations - MenuButton component initial commit - CSS style fixes & improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://michaelgee.com',
 	integrations: [
+		react(),
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp'
 		}),
