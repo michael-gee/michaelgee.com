734ae47be46175b5c0c55e29911c9aae8503768f
Michael Gee <michaelgee221@gmail.com>
2024-02-06 14:03:56 -0500

    add og:image template and images for existing blog posts - add conditional logic for including og:image in BaseHead component

---
+++ src/assets/blog/og-image-template.jsx
@@ -0,0 +1,80 @@
+<div
+	style={{
+		display: 'flex',
+		height: '100%',
+		width: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		letterSpacing: '-.02em',
+		fontWeight: 700,
+		background: '#000000'
+	}}
+>
+	<div
+		style={{
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: '85%',
+			width: '90%',
+			background: '#18181C',
+			borderRadius: '1rem',
+			border: '2px solid #3f3f4666'
+		}}
+	>
+		<div
+			style={{
+				left: 32,
+				top: 32,
+				position: 'absolute',
+				display: 'flex',
+				alignItems: 'center',
+				color: 'white'
+			}}
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+				<path
+					fill="#dc2626"
+					d="M21.79 13L16 16l1 2l-4-.75V21h-2v-3.75L7 18l1-2l-5.79-3l1-1.73L1.61 8l3.6-.23l1-1.77l3.42 3.9L8 5h2l2-3l2 3h2l-1.63 4.9L17.79 6l1 1.73l3.6.23l-1.6 3.23z"
+				/>
+			</svg>
+			<span
+				style={{
+					marginLeft: 8,
+					fontSize: 24
+				}}
+			>
+				michaelgee.com
+			</span>
+		</div>
+		<div
+			style={{
+				display: 'flex',
+				flexWrap: 'wrap',
+				justifyContent: 'center',
+				padding: '20px',
+				margin: '0 42px',
+				fontSize: 40,
+				width: 'auto',
+				maxWidth: 700,
+				textAlign: 'center',
+				color: 'white',
+				lineHeight: 1.4
+			}}
+		>
+			{'<Insert Blog Title Here>'}
+		</div>
+		<div
+			style={{
+				right: 32,
+				bottom: 32,
+				position: 'absolute',
+				display: 'flex',
+				alignItems: 'center',
+				color: '#A1A1AA'
+			}}
+		>
+			{'<Insert Blog Date Here>'}
+		</div>
+	</div>
+</div>;
